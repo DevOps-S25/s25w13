ba318eb1a9b0914fe95f065b761ef5cb207c04ad
Imran Ashraf <zsaing.ucp@gmail.com>
2025-06-09 18:09:29 +0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git config --local user.name "Imran Ashraf"
+git config --local user.email "zsaing.ucp@gmail.com"
+git config --local core.editor notepad
+mkdir ./src ./styles ./assests ./bin ./node_modules
+touch ./src/index.html ./styles/index.css .gitignore
+mkdir -p ./.github/workflows
